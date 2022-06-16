1a183f15dc450c8848653f87b17805843eb61787
unknown <wasimhyder1@gmail.com>
2022-06-16 12:02:24 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Wasim Human Resource Management System</h1>
+      <button >Normal Button</button>
+      <Button >Bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
